f02d3ef34996712462591033a6f96143d1c30393
David <zhidongzhang2015@gmail.com>
2017-12-01 00:18:07 -0500

    dont copy

--- starter/mem.c
+++ starter/mem.c
@@ -249,7 +249,7 @@ void print_all_nodes_information_best(){
 		node_count++;
 		printf("========== Information about Node %d ========\n ", node_count);
 		printf("Current address: %d\n", (long)current_block-(long)best_head);
-		printf("Previous node's' address: %d\n", current_block->prev);
+		printf("Previous node's' address: %d\n", (long)current_block->prev -(long)best_head);
 		printf("Next node's' address: %d\n", current_block->next);
 		printf("size_of_this_memory block: %d\n", current_block->block_size);
 		printf("is_allocated: %d\n", current_block->allocated);
