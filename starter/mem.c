fb382a445499ece368d6d37014bcdc67499b7552
David <zhidongzhang2015@gmail.com>
2017-12-01 02:02:37 -0500

    dont copy

--- starter/mem.c
+++ starter/mem.c
@@ -339,6 +339,7 @@ while (1) {
 
 
 	best_fit_dealloc(eight_1);
+	print_all_nodes_information_best();
 	best_fit_dealloc(thirty_two_1);
 	print_all_nodes_information_best();
 	best_fit_dealloc(one_two_eight_1);
