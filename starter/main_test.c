3bfb4032f3195a3776556278c867677141890ef1
David <zhidongzhang2015@gmail.com>
2017-12-01 20:22:48 -0500

    dont copy

--- starter/main_test.c
+++ starter/main_test.c
@@ -32,18 +32,69 @@ int main(int argc, char *argv[])
 		fprintf(stderr, "Invalid argument, please specify 0 or 1\n");
 		exit(1);
 	}
-	
 	if ( algo == 0 ) {
-		best_fit_memory_init(1024);	// initizae 1KB, best fit
+		best_fit_memory_init(4096);
+	
+while (1) {
+	//print_all_nodes_information_best();
+	void* four_1 =	best_fit_alloc(5);
+	printf("The return value of four_1 is: %d\n", four_1);
+	if (four_1 == NULL) {  break; }
+	
+	//print_all_nodes_information_best();
+	void* two_five_six_1 = best_fit_alloc(256);
+  	printf("The return value of two_five_six_1 is: %d\n", two_five_six_1);
+	if (two_five_six_1 == NULL) { break; }
+	//print_all_nodes_information_best();
+	void* eight_1 = best_fit_alloc(9);
+	printf("The return value of eight_1 is: %d\n", eight_1);
+	if (eight_1 == NULL) { break; }
+	//print_all_nodes_information_best();
+	void* one_two_eight_1 = best_fit_alloc(128);
+	printf("The return value of one_two_eight_1 is: %d\n", one_two_eight_1);
+	if (one_two_eight_1 == NULL) { break; }
+	
+ 	void* sixteen_1 = best_fit_alloc(17);
+ 	printf("The return value of sixteen_1 is: %d\n", sixteen_1);
+	if (sixteen_1 == NULL) { break; }
 
-		p = best_fit_alloc(8);		// allocate 8B
-		printf("best fit: p=%p\n", p);
-		if ( p != NULL ) {
-			best_fit_dealloc(p);	
-		}
-		num = best_fit_count_extfrag(4);
+	void* sixty_four_1 = best_fit_alloc(65);
+ 	printf("The return value of sixty_four_1 is: %d\n", sixty_four_1);
+	if (sixty_four_1 == NULL) { break; }
+	
+ 	void* thirty_two_1 = best_fit_alloc(33);
+ 	printf("The return value of thirty_two_1 is: %d\n", thirty_two_1);
+	if (thirty_two_1 == NULL) { break; }
+
+
+	best_fit_dealloc(eight_1);
+	//print_all_nodes_information_best();
+	best_fit_dealloc(thirty_two_1);
+	//print_all_nodes_information_best();
+	best_fit_dealloc(one_two_eight_1);
+	//print_all_nodes_information_best();
+}
+
+print_all_nodes();
+
+int four_bf, eight_bf, sixteen_bf, thirtytwo_bf, sixtyfour_bf, onetwentyeight_bf, twofiftysix_bf;
+
+four_bf = best_fit_count_extfrag(4);
+eight_bf = best_fit_count_extfrag(8);
+sixteen_bf = best_fit_count_extfrag(16);
+thirtytwo_bf = best_fit_count_extfrag(32);
+sixtyfour_bf = best_fit_count_extfrag(64);
+onetwentyeight_bf = best_fit_count_extfrag(128);
+twofiftysix_bf = best_fit_count_extfrag(256);
+
+printf("the extern frag for best fit for four bytes is %d\n", four_bf);
+printf("the extern frag for best fit for eight bytes is %d\n", eight_bf);
+printf("the extern frag for best fit for 16 bytes is %d\n", sixteen_bf);
+printf("the extern frag for best fit for 32 bytes is %d\n", thirtytwo_bf);
+printf("the extern frag for best fit for 64 bytes is %d\n", sixtyfour_bf);
+printf("the extern frag for best fit for 128 bytes is %d\n", onetwentyeight_bf);
+printf("the extern frag for best fit for 256 bytes is %d\n", twofiftysix_bf);
 	} else if ( algo == 1 ) {
-
 		worst_fit_memory_init(1024);	// initizae 1KB, worst fit
 
 		q = worst_fit_alloc(8);		// allocate 8B
@@ -60,4 +111,10 @@ int main(int argc, char *argv[])
 	printf("num = %d\n", num);
 
 	return 0;
+
+	
+
+
+	return 0;
 }
+
--- starter/mem.c
+++ starter/mem.c
@@ -391,7 +391,7 @@ int worst_fit_count_extfrag(size_t size)
 
 }
 
-void print_all_nodes(){
+void print_all_nodes_best(){
 	Node_block* current_block;
 	current_block = best_head;
 	
@@ -415,112 +415,27 @@ void print_all_nodes(){
 	printf("Size of a struct node: %d\n\n", sizeof(Node_block));
 }
 
-
-int main(int argc, char *argv[])
-{
-	// int num = 0;
-	// int algo = 0; // default algorithm to test is best fit  
-	// void *p, *q;
-
-	// if (argc != 2) {
-	// 	fprintf(stderr, "Usage: %s <0/1>. 0 for best fit and 1 for worst fit \n", argv[0]);
-	// 	exit (1);
-	// } else if (!strcmp(argv[1], "1") || !strcmp(argv[1], "0")) {
-	// 	algo = atoi(argv[1]);
-	// } else {
-	// 	fprintf(stderr, "Invalid argument, please specify 0 or 1\n");
-	// 	exit(1);
-	// }
-	// if ( algo == 0 ) {
-	// 	best_fit_memory_init(1024);	// initizae 1KB, best fit
-	// 	int a;
-	// 	for( a = 0; a < 20; a++ ){
-	// 		p = best_fit_alloc(a);		// allocate 8B
-	// 		//printf("best fit: p=%p\n", p);
-	// 		printf("best fit: p=%d\n", p);
-	// 	}
-	// 	num = best_fit_count_extfrag(4);
-	// } else if ( algo == 1 ) {
-	// 	worst_fit_memory_init(1024);	// initizae 1KB, worst fit
-
-	// 	q = worst_fit_alloc(8);		// allocate 8B
-	// 	printf("worst fit: q=%p\n", q);
-	// 	if ( q != NULL ) {
-	// 		worst_fit_dealloc(q);	
-	// 	}
-	// 	num = worst_fit_count_extfrag(4);
-	// } else {
-	// 	fprintf(stderr, "Should not reach here!\n");
-	// 	exit(1);
-	// }
-
-	// printf("num = %d\n", num);
-
-	// return 0;
-
-	best_fit_memory_init(4096);
+void print_all_nodes_worst(){
+	Node_block* current_block;
+	current_block = worst_head;
 	
-while (1) {
-	//print_all_nodes_information_best();
-	void* four_1 =	best_fit_alloc(5);
-	printf("The return value of four_1 is: %d\n", four_1);
-	if (four_1 == NULL) {  break; }
+	printf("\n========================= PRINTING ALL NODES INFO ===========================\n\n");
 	
-	//print_all_nodes_information_best();
-	void* two_five_six_1 = best_fit_alloc(256);
-  	printf("The return value of two_five_six_1 is: %d\n", two_five_six_1);
-	if (two_five_six_1 == NULL) { break; }
-	//print_all_nodes_information_best();
-	void* eight_1 = best_fit_alloc(9);
-	printf("The return value of eight_1 is: %d\n", eight_1);
-	if (eight_1 == NULL) { break; }
-	//print_all_nodes_information_best();
-	void* one_two_eight_1 = best_fit_alloc(128);
-	printf("The return value of one_two_eight_1 is: %d\n", one_two_eight_1);
-	if (one_two_eight_1 == NULL) { break; }
+	int count = 0;
 	
- 	void* sixteen_1 = best_fit_alloc(17);
- 	printf("The return value of sixteen_1 is: %d\n", sixteen_1);
-	if (sixteen_1 == NULL) { break; }
-
-	void* sixty_four_1 = best_fit_alloc(65);
- 	printf("The return value of sixty_four_1 is: %d\n", sixty_four_1);
-	if (sixty_four_1 == NULL) { break; }
-	
- 	void* thirty_two_1 = best_fit_alloc(33);
- 	printf("The return value of thirty_two_1 is: %d\n", thirty_two_1);
-	if (thirty_two_1 == NULL) { break; }
-
-
-	best_fit_dealloc(eight_1);
-	//print_all_nodes_information_best();
-	best_fit_dealloc(thirty_two_1);
-	//print_all_nodes_information_best();
-	best_fit_dealloc(one_two_eight_1);
-	//print_all_nodes_information_best();
-}
-
-print_all_nodes();
-
-int four_bf, eight_bf, sixteen_bf, thirtytwo_bf, sixtyfour_bf, onetwentyeight_bf, twofiftysix_bf;
-
-four_bf = best_fit_count_extfrag(4);
-eight_bf = best_fit_count_extfrag(8);
-sixteen_bf = best_fit_count_extfrag(16);
-thirtytwo_bf = best_fit_count_extfrag(32);
-sixtyfour_bf = best_fit_count_extfrag(64);
-onetwentyeight_bf = best_fit_count_extfrag(128);
-twofiftysix_bf = best_fit_count_extfrag(256);
-
-printf("the extern frag for best fit for four bytes is %d\n", four_bf);
-printf("the extern frag for best fit for eight bytes is %d\n", eight_bf);
-printf("the extern frag for best fit for 16 bytes is %d\n", sixteen_bf);
-printf("the extern frag for best fit for 32 bytes is %d\n", thirtytwo_bf);
-printf("the extern frag for best fit for 64 bytes is %d\n", sixtyfour_bf);
-printf("the extern frag for best fit for 128 bytes is %d\n", onetwentyeight_bf);
-printf("the extern frag for best fit for 256 bytes is %d\n", twofiftysix_bf);
-
-
-	return 0;
+	// traverse the list
+	while(current_block){
+		printf("========== Information about Node %d ========\n ", count);
+		printf("Current address: %d\n", (long)current_block-(long)worst_head);
+		printf("Previous node's' address: %d\n", (long)current_block->prev -(long)worst_head);
+		printf("Next node's' address: %d\n", current_block->next);
+		printf("size_of_this_memory block: %d\n", current_block->block_size);
+		printf("is_allocated: %d\n", current_block->allocated);
+		current_block = current_block->next;
+		count++;
+	}
+	printf("========== General Stuff ========\n ");
+	printf("Total number of nodes: %d\n", count);
+	printf("Size of a struct node: %d\n\n", sizeof(Node_block));
 }
 
