26b922499d343d3c9476574c88ed0e5afca9495d
David <zhidongzhang2015@gmail.com>
2017-12-02 22:08:46 -0500

    dont copy

--- starter/main_test.c
+++ starter/main_test.c
@@ -214,7 +214,7 @@ int main(int argc, char *argv[])
 		printf("Worst fit test 5 Passed\n");
 
 		printf("\n========================= Worst fit test 6: External fragmentation test ===========================\n\n");
-		worst_fit_memory_init(10240);
+		worst_fit_memory_init(102400);
 		int i;
 		while(1){
 			//Alloc
