5aff1b80b60e7c67da6f60fad3138f41ff474b17
David <zhidongzhang2015@gmail.com>
2017-12-03 02:12:10 -0500

    dont copy

--- starter/main_test.c
+++ starter/main_test.c
@@ -212,7 +212,7 @@ int main(int argc, char *argv[])
 		q = (void*)((size_t)p +1);
 		worst_fit_dealloc(q);
 		//block that has is not allocated, so dealloc will fail
-		q = (void*)((size_t)p + sizeof(Node_block) + 5);
+		q = (void*)((size_t)p + 32 + 5);
 		worst_fit_dealloc(q);
 		printf("Worst fit test 5 Passed\n");
 
